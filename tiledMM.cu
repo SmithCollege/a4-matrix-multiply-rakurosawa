ce4bb239df3f303ec55bee962a826c01fa81edb9
Ronin Akurosawa <rakurosawa@smith.edu>
2024-11-06 14:34:10 -0500

    tiled time tests

--- tiledMM.cu
+++ tiledMM.cu
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <sys/time.h>
 
 #define TILE_WIDTH 16 // 16 * 16 = 256
 
@@ -29,14 +30,27 @@ __global__ void MatMul(float* d_M, float* d_N, float* d_P, int Width){
     }
 }
 
+
+double get_clock() {
+    struct timeval tv; int ok;
+    ok = gettimeofday(&tv, (void *) 0);
+    if (ok<0) { 
+        printf("gettimeofday error"); 
+        }
+    return (tv.tv_sec * 1.0 + tv.tv_usec * 1.0E-6);
+}
+
+
 int main(){
-    int size = 128; // test 128, 256, 512
+    int size = 512; // test 128, 256, 512
 
     // allocate memory for the arrays
     float *x, *y, *z;
     cudaMallocManaged(&x, sizeof(float) * size * size);
     cudaMallocManaged(&y, sizeof(float) * size * size);
     cudaMallocManaged(&z, sizeof(float) * size * size);
+
+    double t0, t1;
 
     // initialize values for x and y arrays
     for (int i = 0; i < size; i++) {
@@ -50,16 +64,12 @@ int main(){
     dim3 dimGrid(ceil((1.0*size)/TILE_WIDTH), ceil((1.0*size)/TILE_WIDTH), 1);
     dim3 dimBlock(TILE_WIDTH, TILE_WIDTH, 1);
 
+    t0 = get_clock();
     MatMul<<<dimGrid, dimBlock>>>(x, y, z, size);
     cudaDeviceSynchronize();
+    t1 = get_clock();
 
-    // // print the output for small size testing
-    // for (int i = 0; i < size; i++){
-    //     for (int j = 0; j < size; j++){
-    //         printf("[%f] ", z[i * size + j]);
-    //     }
-    //     printf("\n");
-    // }
+    printf("time: %f ns\n", 1000000000.0*(t1-t0));
 
     // check for errors (presumes array is only 1's)
     for (int i = 0; i < size; i++) {
